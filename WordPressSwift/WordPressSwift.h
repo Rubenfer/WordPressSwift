c53b1b40e3f5a33aa7778dda18c9a798f0f28098
Ruben Fernandez <rubenfernandez@Mac-mini-de-Ruben.local>
2018-03-05 16:05:09 +0100

    Initial Commit

---
+++ WordPressSwift/WordPressSwift.h
@@ -0,0 +1,19 @@
+//
+//  WordPressSwift.h
+//  WordPressSwift
+//
+//  Created by Ruben Fernandez on 5/3/18.
+//  Copyright © 2018 Ruben Fernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WordPressSwift.
+FOUNDATION_EXPORT double WordPressSwiftVersionNumber;
+
+//! Project version string for WordPressSwift.
+FOUNDATION_EXPORT const unsigned char WordPressSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WordPressSwift/PublicHeader.h>
+
+
